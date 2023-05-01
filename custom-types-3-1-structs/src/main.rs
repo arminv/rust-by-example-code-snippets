73150b47c7b6613804a80f73bd955ae4e40db334
Armin Varshokar <arminvarshokar@gmail.com>
2023-05-01 00:33:48 -0400

    3-1-Structs done

--- custom-types-3-1-structs/src/main.rs
+++ custom-types-3-1-structs/src/main.rs
@@ -14,12 +14,14 @@ struct Unit;
 struct Pair(i32, f32);
 
 // A struct with two fields
+#[derive(Debug)]
 struct Point {
     x: f32,
     y: f32,
 }
 
 // Structs can be reused as fields of another struct
+#[derive(Debug)]
 struct Rectangle {
     // A rectangle can be specified by where the top left and bottom right
     // corners are in space.
@@ -62,7 +64,7 @@ fn main() {
             x: left_edge,
             y: top_edge,
         },
-        bottom_right: bottom_right,
+        bottom_right, // shorthand - same as: bottom_right: bottom_right
     };
 
     // Instantiate a unit struct
@@ -78,4 +80,48 @@ fn main() {
     let Pair(integer, decimal) = pair;
 
     println!("pair contains {:?} and {:?}", integer, decimal);
+
+    println!(
+        "Rectangle Area is: {}",
+        rect_area(Rectangle {
+            top_left: (Point { x: 0.0, y: 10.0 }),
+            bottom_right: (Point { x: 5.0, y: 0.0 })
+        })
+    );
+
+    println!(
+        "Rectangle with a top-right edge of (0.0, 0.0) and width of 5.0 is represented by: {:?}",
+        square(Point { x: 0.0, y: 0.0 }, 5.0)
+    );
+}
+
+fn rect_area(rectangle: Rectangle) -> f32 {
+    let Rectangle {
+        top_left,
+        bottom_right,
+    } = rectangle;
+
+    let width = (bottom_right.x - top_left.x).abs();
+    let height = (top_left.y - bottom_right.y).abs();
+
+    let area = width * height;
+
+    area
+}
+
+fn square(top_left_point: Point, width: f32) -> Rectangle {
+    if width < 0.0 {
+        panic!("Width cannot be negative!");
+    }
+
+    let bottom_right_x = top_left_point.x + width;
+    let bottom_right_y = top_left_point.y - width;
+
+    Rectangle {
+        top_left: top_left_point,
+        bottom_right: Point {
+            x: bottom_right_x,
+            y: bottom_right_y,
+        },
+    }
 }
